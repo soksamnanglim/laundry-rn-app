33d4ef2fe36f5e52c9c9600a66486bc87e428fa7
carmenli14 <cal009@ucsd.edu>
2020-09-29 01:05:37 -0700

    added signin

--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -6,7 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import SplashScreen from './SplashScreen';
 import SignInScreen from './SignInScreen';
-import SignUpScreen from './SignUpScreen';
+import SignUpScreen from './SignUpScreen.js';
 
 const RootStack = createStackNavigator();
 
